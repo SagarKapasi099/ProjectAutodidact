1ebf89fcdfb51b6815ce9a17e0a6fdee6a3de133
Sagar Kapasi <SagarKapasi099@gmail.com>
2019-04-28 15:22:41 +0530

    Cleared BitTorrentClient

--- src/main/java/SagarKapasi099/ProjectAutodidact/BitTorrentClient/BitTorrentClient.java
+++ src/main/java/SagarKapasi099/ProjectAutodidact/BitTorrentClient/BitTorrentClient.java
@@ -1,18 +1,5 @@
 package SagarKapasi099.ProjectAutodidact.BitTorrentClient;
 
-import com.turn.ttorrent.client.Client;
-import com.turn.ttorrent.client.SharedTorrent;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.InetAddress;
-import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-
-/**
- * This Class manages an ArrayList of {@link com.turn.ttorrent.client.Client}
- */
-
 public class BitTorrentClient {
 
 }
